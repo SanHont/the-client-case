ed027f597fc1fedda96e955af8f90544241abc0a
SanHont <dehontsander@gmail.com>
2022-09-30 09:50:58 +0200

    Content toegevoegd
    Ik heb al een groot deel van de content voor de pagina toegevoegd op de site. Ook heb ik een accordion toegevoegd. Hierdoor kan ik de content mooi georganiseerd houden.

--- scripts/script.js
+++ scripts/script.js
@@ -24,3 +24,18 @@ function showSlides() {
   slides[slideIndex - 1].style.display = "block";
   setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
