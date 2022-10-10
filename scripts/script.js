28dd76fd9b65e3987edd7ec8ea8e845a6c608898
SanHont <dehontsander@gmail.com>
2022-10-10 18:37:48 +0200

    Content Update
    Als het goed is staat nu alle content op mijn website, ik moet nog wel aanpassingen maken in de css maar het grootste deel met content is al gedaan.

--- scripts/script.js
+++ scripts/script.js
@@ -22,7 +22,7 @@ function showSlides() {
     slideIndex = 1;
   }
   slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 3000); // Change image every 2 seconds
+  setTimeout(showSlides, 3000);  // Change image every 2 seconds
 }
 
 var acc = document.getElementsByClassName("accordion");
